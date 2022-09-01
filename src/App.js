3f652520cda7b33bb661d5f6516d418605e30670
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2022-09-01 22:08:19 +0900

    6.1 useEffect
    useEffect first argument

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import Button from "./Button";
-import styles from "./App.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+//useEffect는 두개의 argument를 가지는 function
+//첫번째 argument: 딱 한번만 실행할 코드
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  //state를 변경할 때 모든 코드들은 항상 다시 실행된다.
-  //가끔 component 내부의 몇몇 코드는 처음 한번만 실행되고 다시는 실행되도록 하고 싶지 않을때 --> 6.1내용
-  console.log("call an api");
+  console.log("I run all the time");
+  /*const iRunOnlyOnce = () => {
+    console.log("i run only once.");
+  };
+  useEffect(iRunOnlyOnce, []);*/
+  useEffect(() => {
+    console.log("call the api...");
+  }, []);
   return (
     <div>
       <h1>{counter}</h1>
