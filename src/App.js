1a92497441264ee4fed57b3a88b0d4d751a49695
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2022-09-01 22:40:19 +0900

    6.2 Deps
    useEffect secound argument

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import { useState, useEffect } from "react";
-//useEffect는 두개의 argument를 가지는 function
-//첫번째 argument: 딱 한번만 실행할 코드
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("I run all the time");
-  /*const iRunOnlyOnce = () => {
-    console.log("i run only once.");
+  const onChange = (event) => {
+    setKeyword(event.target.value);
   };
-  useEffect(iRunOnlyOnce, []);*/
   useEffect(() => {
-    console.log("call the api...");
+    console.log("I run only once.");
   }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes");
+  }, [counter]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
