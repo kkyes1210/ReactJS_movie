05291c4d489b41a09694900b1963acd50666630f
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2022-09-01 21:54:17 +0900

    6.0 Introduction

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  //state를 변경할 때 모든 코드들은 항상 다시 실행된다.
+  //가끔 component 내부의 몇몇 코드는 처음 한번만 실행되고 다시는 실행되도록 하고 싶지 않을때 --> 6.1내용
+  console.log("call an api");
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
