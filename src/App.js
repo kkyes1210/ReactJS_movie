d923da209b63707a9d06e9c794c97655552d2945
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2022-09-05 16:55:36 +0900

    7.5 React Router
    router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import { useEffect, useState } from "react";
-import Movie from "./components/Movie";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  return null;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,17 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+//Link는 브라우저 새로고침 없어도 유저를 다른 페이지로 이동시켜주는 컴포넌트
+//<a> 태그는 브라우저 새로고침이 되기 때문에 Link를 사용하는 것이 좋다 !
+//a href="" ==> Link to=""
 
 function Movie({ coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
-      <h2>{title}</h2>
+      <h2>
+        <Link to="/movie">{title}</Link>
+      </h2>
       <p>{summary}</p>
       <ul>
         {genres.map((g) => (
