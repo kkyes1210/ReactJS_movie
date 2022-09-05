c2e558404e228c085235879c3b05b329240a15ee
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2022-09-05 15:41:11 +0900

    7.4 Movie App part Two

--- src/App.js
+++ src/App.js
@@ -1,50 +1,7 @@
 import { useEffect, useState } from "react";
+import Movie from "./components/Movie";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minumum_rating=8.5&sort_by=year`
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map((g) => (
-                  <li key={g}>{g}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return null;
 }
 export default App;
-
-/*
-const response = await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minumum_rating=8.5&sort_by=year`
-    );
-const json = await response.json();
-코드와 동일
- */
